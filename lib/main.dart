6aca4bf67497d601a53da619ed85213da368e3e1
curkirk <kirk.who.knew.eve.world@gmail.com>
2022-02-22 22:29:21 +0700

    kirk edition

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
         
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter'),
+      home: const MyHomePage(title: 'Flutter Hmmmm.'),
     );
   }
 }
